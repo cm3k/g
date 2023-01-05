ae6b1223c50cdf63d3a67b19b24781803466a209
Thomas CashMan <48776071+cm3k@users.noreply.github.com>
2023-01-05 03:46:27 +0000

    added n64

---
+++ n64/romlist.js
@@ -0,0 +1,7 @@
+var ROMLIST = [
+    {url:"roms/fifa99.z64",title:"FIFA 99"},
+    {url:"roms/supersmashbros.v64",title:"Super Smash Bros"},
+    {url:"roms/mario64.v64",title:"Mario 64"},
+    {url:"roms/mario64.v64",title:"Mario Kart 64"},
+    {url:"roms/goldeneye007.v64",title:"GoldenEye 007"},
+];
